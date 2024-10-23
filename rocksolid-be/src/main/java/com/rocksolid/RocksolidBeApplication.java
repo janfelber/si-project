0a946f3062d669f1b8c184583fd6c58cdde784ac
Ján Felber <jan.felber.mail@gmail.com>
2024-10-23 10:50:58 +0200

    init be commit
    init be commit

---
+++ rocksolid-be/src/main/java/com/rocksolid/RocksolidBeApplication.java
@@ -0,0 +1,13 @@
+package com.rocksolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocksolidBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocksolidBeApplication.class, args);
+	}
+
+}
---
+++ rocksolid-be/src/test/java/com/rocksolid/RocksolidBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocksolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocksolidBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
