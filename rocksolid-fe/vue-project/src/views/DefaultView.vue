ab8970978da431d74bc79daf867de9973f54b95a
Ján Felber <jan.felber.mail@gmail.com>
2024-11-23 18:42:08 +0100

    ROC-21 Sidenav

---
+++ rocksolid-fe/vue-project/src/views/DefaultView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="default-layout">
+    <header class="header">
+    </header>
+    <div class="content">
+      <side-nav/>
+      <main class="main">
+        <router-view/>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNav from "@/components/Sidenav.vue";
+import {RouterView} from "vue-router";
+
+export default {
+  name: "DefaultLayout",
+  components: {RouterView, SideNav},
+};
+</script>
+
+<style scoped>
+.main {
+  margin-left: 250px;
+  padding: 20px;
+}
+
+</style>
