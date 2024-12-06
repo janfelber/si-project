e721eaea01497c45f52d9ab4f13f9a64694d130a
Marcel Felber <marcel.felber.mail@gmail.com>
2024-12-06 18:05:11 +0100

    ROC-42 Import Flyway framework

---
+++ rocksolid-be/src/main/resources/db/migration/V1__Ignored_migration.sql
@@ -0,0 +1,4 @@
+-- This file contains no migration.
+-- Its purpose is solely to maintain the correct migration numbering for us,
+-- as the actual first migration is handled by the Flyway baseline during migration execution,
+-- so we need to start with V2.
