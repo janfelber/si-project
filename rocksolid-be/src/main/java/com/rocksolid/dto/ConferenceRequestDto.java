eaf5660cbe5dc4284fb79fc1723d33faa814d801
Marcel Felber <marcel.felber.mail@gmail.com>
2025-01-16 09:43:29 +0100

    ROC-119 Update UI for conference table

--- rocksolid-be/src/main/java/com/rocksolid/dto/ConferenceRequestDto.java
+++ rocksolid-be/src/main/java/com/rocksolid/dto/ConferenceRequestDto.java
@@ -6,10 +6,8 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class ConferenceRequestDto {
-    private Long article_id;
     private String date_from;
     private String date_to;
-    private Long form_id;
     private int year;
     private String name;
     private String description;
